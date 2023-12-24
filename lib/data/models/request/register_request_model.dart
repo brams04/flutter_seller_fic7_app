4b3b391543678aae83d70b10ffdd01c5aec25064
brams04 <putratoang@gmail.com>
2023-12-24 18:31:44 +0700

    meet 14

--- lib/data/models/request/register_request_model.dart
+++ lib/data/models/request/register_request_model.dart
@@ -2,9 +2,9 @@ import 'dart:convert';
 
 class RegisterRequestModel {
 //   {
-//     "email":"saiful@gmail.com",
+//     "email":"bram@gmail.com",
 //     "password": "12345678",
-//     "name":"saiful"
+//     "name":"bram"
 // }
 
   final String email;
